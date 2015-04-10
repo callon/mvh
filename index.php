6aa1bb719289ead1c3acbd3c8a0bc73870046c9e
callon <rafhama@gmail.com>
2015-04-10 18:47:58 +0200

    Link styling
    Link styling

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
 <main class="container">
-	<section class="last_project_promo"></section>	
-	<section class="about_promo"></section>
-	<section class="earlier_projects_promo"></section>
-	<section class="actor_records_promo"></section>	
+	<section class="last-project-promo"></section>	
+	<section class="about-promo"></section>
+	<section class="earlier-projects-promo"></section>
+	<section class="actor-records-promo"></section>	
 </main>
 
 <?php get_footer(); ?>
