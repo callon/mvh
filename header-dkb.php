373a006f39f71ef04497bfbe734eed3fefef7920
Mads Nedergaard <nedergaardmads@gmail.com>
2015-05-03 19:05:50 +0200

    Update header-dkb.php

--- header-dkb.php
+++ header-dkb.php
@@ -5,6 +5,7 @@
 	<title><?php bloginfo('description'); ?></title>
 	<meta name="description" content="">
 	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/materialize.min.css">
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/magnific-popup.css">
